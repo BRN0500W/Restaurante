c8777f29f268a42dcaa4cf0535a1815626547b7d
Lucas Molina Carrijo <lmcarrijo06@gmail.com>
2026-05-15 19:30:33 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function validarPedido() {
-    let nome = document.getElementById("nome").value;
-    
-    if (nome === "") {
-        alert("Digite seu nome!");
-        return false;
-    }
-    return true;
-}
